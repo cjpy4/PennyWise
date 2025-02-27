86d55edcf46c49890bf3a7ae460abc37095d7f22
Landan Quartemont <landan@grease.solutions>
2025-02-27 10:40:28 -0600

    Updated code for dynamic transactions

--- db.go
+++ db.go
@@ -6,10 +6,12 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
 	"github.com/joho/godotenv"
 	"github.com/tursodatabase/go-libsql"
 )
- func connectToDB() (*libsql.Connector, *sql.DB, string, error) {
+
+func connectToDB() (*libsql.Connector, *sql.DB, string, error) {
 	// Load .env file
 	err := godotenv.Load()
 	if err != nil {
@@ -35,7 +37,6 @@ import (
 		libsql.WithSyncInterval(syncInterval),
 	)
 
-	
 	if err != nil {
 		return nil, nil, dir, fmt.Errorf("error creating connector: %w", err)
 	}
@@ -44,4 +45,71 @@ import (
 
 	return connector, db, dir, nil
 
- }
+}
+
+func queryDB(db *sql.DB, query string) []map[string]any {
+	rows, err := db.Query(query)
+	if err != nil {
+		// If the query execution fails, print the error message to standard error and exit.
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	// Ensure that the rows are closed after we finish processing them.
+	defer rows.Close()
+
+	// Instantiate JSON rows
+	rowJSON := make([]map[string]any, 0)
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		fmt.Println("Error getting columns:", err)
+		return nil // Return nil to indicate an error
+	}
+
+	// Iterate over the rows returned by the query.
+	for rows.Next() {
+		// Create a slice to hold the values for each column
+		values := make([]any, len(columns))
+		// Create a map to hold the column name and value pairs
+		rowMap := make(map[string]any)
+
+		// Scan the columns into the values slice
+		if err := rows.Scan(values...); err != nil {
+			// If scanning fails, print an error message and return from the function.
+			fmt.Println("Error scanning row:", err)
+			return nil
+		}
+
+		// Populate the rowMap with column names and their corresponding values
+		for i, colName := range columns {
+			rowMap[colName] = values[i]
+		}
+
+		// Append the rowMap to rowJSON
+		rowJSON = append(rowJSON, rowMap)
+	}
+
+	// Check if there were any errors encountered during the row iteration.
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+
+	return rowJSON // Return the JSON rows
+}
+
+func executeSQL(db *sql.DB, stmt string) {
+	result, err := db.Exec(stmt)
+	if err != nil {
+		// If the query execution fails, print the error message to standard error and exit.
+		fmt.Fprintf(os.Stderr, "failed to execute sql: %v\n", err)
+		os.Exit(1)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		fmt.Println("Cannot obtain affected rows", err)
+	}
+	
+	fmt.Println("rows affected: ", rowsAffected)
+}
--- utilities/fileUtilities.go
+++ utilities/fileUtilities.go
@@ -5,11 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // var mySlice []int = []int{1,2,3,4,5,6}
 
-func getFile(filePath string) *os.File {
+func GetFile(filePath string) *os.File {
 	fmt.Println("filePath:",filePath)
 	file, err := os.Open(filePath)
 	if (err != nil) {
@@ -20,7 +21,7 @@ func getFile(filePath string) *os.File {
 }
 
 
-func csvToJSON(csvFile os.File) []map[string]any {
+func CSVToJSON(csvFile os.File) []map[string]any {
 	csvReader := csv.NewReader(&csvFile)
 	records, err := csvReader.ReadAll()
 	if err != nil {
@@ -43,7 +44,30 @@ func csvToJSON(csvFile os.File) []map[string]any {
 	return jsonArray
 }
 
-func stringifyJSON(jsonArray []map[string]any) json.RawMessage {
+func CSVStringToJSON(csvString string) []map[string]any {
+	csvReader := csv.NewReader(strings.NewReader(csvString))
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println("cannot retrieve records from Reader", err.Error())
+	}
+	var headers []string = []string{}
+	var jsonArray []map[string]any
+	for i, r := range records {
+		if (i == 0) {
+			headers = r
+		} else {
+			rowMap := make(map[string]any)
+			for j, v := range r {
+				rowMap[headers[j]] = v
+			}
+			jsonArray = append(jsonArray, rowMap)
+		}
+	}
+
+	return jsonArray
+}
+
+func StringifyJSON(jsonArray []map[string]any) json.RawMessage {
 	output, err := json.Marshal(jsonArray)
 	if (err != nil) {
 		fmt.Println("Cannot encode jsonArray as JSON", err)
